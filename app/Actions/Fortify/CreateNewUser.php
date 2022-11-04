8b2728beaafa03b40b4cf5a0a4ec141bab7ccdfa
Snoker87 <snoker87@hotmail.com>
2022-11-04 20:37:48 +0300

    x

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -25,13 +25,7 @@ class CreateNewUser implements CreatesNewUsers
     {
         logger('You are here');
         $input['name'] = $input['first_name']." ".$input['last_name'];
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'type' => ['required','string'],
-            'password' => $this->passwordRules(),
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-        ])->validate();
+        logger($input);
         logger('You are here after the validaion');
 
         return DB::transaction(function () use ($input) {
